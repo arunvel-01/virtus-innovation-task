f2b3b107e16da50ee078730a17fc34b58b2dcd34
Arun Vel <arunvelv9253@gmail.com>
2024-09-07 23:41:20 +0530

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 
 // import Car from "./Components/Car";
 
-// export default function Home() {
-//   return (
-//     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-//       <Car />
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      {/* <Car /> */}
         
-//     </div>
-//   );
-// }
+    </div>
+  );
+}
